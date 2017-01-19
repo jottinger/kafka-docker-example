d95493f42dd5ce7918776eeadbdb50ac8bae1c82
Joseph B. Ottinger <joseph.ottinger@meltwater.com>
2017-01-19 18:58:42 -0500

    add poll capability to JS queue, update pyconsumer for unique clientid

--- consumer/app/app.js
+++ consumer/app/app.js
@@ -3,30 +3,13 @@ const config     = require ('konphyg') (__dirname + '/../config') ('mq-config');
 const winston    = require ('winston');
 const app        = express ();
 const bodyParser = require ('body-parser');
-const Kafka      = require ('no-kafka');
-const Promise    = require ('bluebird');
-const consumer   = new Kafka.GroupConsumer ({
-  clientId: config[config.connector].clientId,
-  groupId: config[config.connector].groupId,
-  connectionString: config[config.connector].brokers
-});
+const Queue      = require ('./queue');
 
-const dataHandler = function (messageSet, topic, partition) {
-  return Promise.each (messageSet,
-    (m) => {
-      winston.info (JSON.stringify ({
-        topic: topic,
-        partition: partition,
-        offset: m.offset,
-        message: m.message.value.toString ('utf8')
-      }));
-      // do something with the message here
-      return consumer.commitOffset ({topic: topic, partition: partition, offset: m.offset, metadata: 'optional'});
-    }
-  );
-};
+winston.info (JSON.stringify (config.connector, null, 2));
+winston.info (Queue.QueueTypes);
+winston.info ('queuetypes:' + JSON.stringify (Queue.QueueTypes, null, 2));
 
-let strategies=null;
+let destination;
 
 app.use (bodyParser.json ());
 
@@ -37,12 +20,10 @@ app.get ('/', function (req, res) {
 app.listen (3000, function () {
   winston.info ('Example app listening on port 3000 in ' + config.mode + ' mode!');
   setTimeout (function () {
-
     winston.info ("setting up listener");
-    strategies = [{
-      subscriptions: [config[config.connector].input],
-      handler: dataHandler
-    }];
-    consumer.init(strategies);
+    destination = new Queue.QueueTypes[config.connector] (config[config.connector]);
+    destination.poll ([config[config.connector].input],
+      (m) => {
+      });
   }, 6000);
 });
--- pyconsumer/code/pyconsumer.py
+++ pyconsumer/code/pyconsumer.py
@@ -2,14 +2,19 @@ import pprint
 from kafka import KafkaConsumer
 import time
 import json
+import uuid
 
 print "hello, pyconsumer"
 time.sleep(25) # delays for 5 seconds
 
 print "PYCONSUMER IS ABOUT TO RUN ----------------------------"
 # To consume latest messages and auto-commit offsets
+consumerGroupId='py'+str(uuid.uuid1())
+print consumerGroupId
 consumer = KafkaConsumer('sample-topic',
+                         api_version=(0,9),
                          group_id='pyconsumer',
+                         client_id=consumerGroupId,
                          bootstrap_servers=['kafkabroker:9092'],
                          value_deserializer=lambda m: json.loads(m.decode('ascii')))
 
